360a50fb71cbbc3fd0629c34b8646c3cbe577943
Sean Doran <me@spjpgrd.com>
2022-12-27 22:38:21 -0700

    🎨 Format all files with npx prettier --write .

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -4,15 +4,15 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
-  name: 'default',
-  title: 'dot-com',
+    name: 'default',
+    title: 'dot-com',
 
-  projectId: '43two8qm',
-  dataset: 'production',
+    projectId: '43two8qm',
+    dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+    plugins: [deskTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
-  },
+    schema: {
+        types: schemaTypes,
+    },
 })
