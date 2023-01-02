f52c6c7e7fa2345b341f963f78ae0d23d2de6a62
Sean Doran <me@spjpgrd.com>
2023-01-01 23:37:46 -0700

    💬 Text for NP and Last.fm source

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -2,26 +2,29 @@ const NowPlaying = (props) => {
   const data = { ...props.data };
   if (Object.keys(data).length != 0) {
     return (
-      <dl>
-        <dt>
-          {data.recenttracks.track[0]["@attr"]?.nowplaying === "true"
-            ? `Now Playing`
-            : `Last Played`}
-        </dt>
-        <br />
-        <dd>
-          <span>
-            <strong>{data.recenttracks.track[0].name}</strong>{" "}
-            <a target="_blank" href={data.recenttracks.track[0].url}>
-              ☞
-            </a>
-          </span>
+      <>
+        <dl>
+          <dt>
+            {data.recenttracks.track[0]["@attr"]?.nowplaying === "true"
+              ? `Now Listening to:`
+              : `Last Listened to:`}
+          </dt>
           <br />
-          <small>
-            <span>{data.recenttracks.track[0].artist.name}</span>
-          </small>
-        </dd>
-      </dl>
+          <dd>
+            <span>
+              <strong>{data.recenttracks.track[0].name}</strong>{" "}
+              <a target="_blank" href={data.recenttracks.track[0].url}>
+                ☞
+              </a>
+            </span>
+            <br />
+            <small>
+              <span>{data.recenttracks.track[0].artist.name}</span>
+            </small>
+          </dd>
+        </dl>
+        <p>Provided by Last.fm</p>
+      </>
     );
   }
 };
