f0383f780396aabc66d7afbc9b54bb1ae9b4fe17
Sean Doran <me@spjpgrd.com>
2023-01-01 23:03:09 -0700

    🎨 Prettier

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -1,11 +1,13 @@
-const data = await fetch('https://ws.audioscrobbler.com/2.0/?api_key=d89f57d26c2715455eda7c4cef52490c&method=User.getrecenttracks&user=spjpgrd&format=json&limit=1&extended=1')
-.then(response => {
+const data = await fetch(
+  "https://ws.audioscrobbler.com/2.0/?api_key=d89f57d26c2715455eda7c4cef52490c&method=User.getrecenttracks&user=spjpgrd&format=json&limit=1&extended=1"
+)
+  .then((response) => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
     return response.json();
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
   });
 
@@ -14,19 +16,26 @@ const NowPlaying = () => {
     return (
       <dl>
         <dt>
-        {data.recenttracks.track[0]['@attr'].nowplaying === "true" ?
-          `Now Playing` : `Last Played`
-          }
+          {data.recenttracks.track[0]["@attr"].nowplaying === "true"
+            ? `Now Playing`
+            : `Last Played`}
         </dt>
         <br />
-      <dd>
-          <span><strong>{data.recenttracks.track[0].name}</strong></span>
+        <dd>
+          <span>
+            <strong>{data.recenttracks.track[0].name}</strong>
+          </span>
           <br />
-          <small><span>{data.recenttracks.track[0].artist.name}</span> <a target="_blank" href={data.recenttracks.track[0].artist.url}>☞</a></small>
-      </dd>
-    </dl >
-    )
+          <small>
+            <span>{data.recenttracks.track[0].artist.name}</span>{" "}
+            <a target="_blank" href={data.recenttracks.track[0].artist.url}>
+              ☞
+            </a>
+          </small>
+        </dd>
+      </dl>
+    );
   }
-}
+};
 
 export default NowPlaying;
