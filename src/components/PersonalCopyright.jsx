0756e0d661153cf5deba23b5214559bdefe7f655
Sean Doran <me@spjpgrd.com>
2023-01-05 21:18:27 -0700

    ✨ Add PersonalCopyright

---
+++ src/components/PersonalCopyright.jsx
@@ -0,0 +1,11 @@
+function PersonalCopyright() {
+    const firstYear = 2022;
+    const currentYear = new Date().getFullYear();
+    const displayedDateRange = `${firstYear}–${currentYear}`
+
+    return (
+        <p>Copyright © <time>{displayedDateRange}</time> SPJPGRD LLC.</p>
+    )
+  };
+
+  export default PersonalCopyright;
