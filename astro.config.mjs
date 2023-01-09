4b1c41c9b403759f1e42eb20350ec98774e34650
Sean Doran <me@spjpgrd.com>
2023-01-08 22:03:36 -0700

    ➕ Add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react()],
+  integrations: [react(), compress()]
 });
